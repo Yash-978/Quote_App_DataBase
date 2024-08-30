751f2c080ea2a218c7b14436414f8613184f0fe1
Yash <147479013+Yash-978@users.noreply.github.com>
2024-08-30 13:59:35 +0530

    Update quoteModal.dart

--- lib/Modal/quoteModal.dart
+++ lib/Modal/quoteModal.dart
@@ -1,26 +1,4 @@
-/*
-class QuoteModal {
-  late String quote, author, cate, like;
-  late int? id;
 
-  QuoteModal({
-    required this.quote,
-    required this.author,
-    required this.cate,
-    required this.like,
-    required this.id,
-  });
-
-  factory QuoteModal.fromJson(Map m1) {
-    return QuoteModal(
-        id: m1['id'],
-        quote: m1['quote'],
-        author: m1['author'],
-        cate: m1['cate'],
-        like: m1['like']);
-  }
-}
-*/
 class QuoteModal {
   late String quote, author, cate, like;
   late int? id;
