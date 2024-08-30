83ac1de8be90112634964d5675d955648f60e71f
Yash <147479013+Yash-978@users.noreply.github.com>
2024-08-30 14:00:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,4 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:quote_app_db/View/Screens/HomeScreen.dart';
 
-import 'View/Screens/QuoteScreen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(debugShowCheckedModeBanner: false,
-      getPages: [
-        GetPage(name: '/', page: () => const HomePage(),),
-        GetPage(name: '/quote', page: () => const QuotePage(),),
-      ],
-
-    );
-  }
-}
-*/
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quote_app_db/View/Screens/HomeScreen.dart';
